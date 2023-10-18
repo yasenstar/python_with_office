252827dee6b9ae3ff5f59b14ddfacc91b90403d5
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2023-10-18 16:53:10 -0400

    add 002 Data Structure - Numbers

---
+++ part1/ch02/2-1.py
@@ -0,0 +1,38 @@
+# Numeric Data Type
+
+# 1. Integer
+
+# res = 64 + 32
+# print(type(res), res)
+# print(64 - 32)
+# print(32 * 2)
+# print((2 + 6) * 2)
+# print(32 / 2)
+# print(32 // 2)
+
+# 2. Floats
+
+# res = 3.6 - 3.1
+# print(type(res), res)
+# print(1.1 - 1)
+# print(0.1 + 0.1 + 0.1)
+# print(0.1 * 3)
+# print(1/3)
+# print(1/4.0)
+
+# 3. Boolean
+
+# is_ok = True
+# print(type(is_ok), is_ok)
+# has_data = False
+# print(type(has_data), has_data)
+
+# Complex Number
+
+a = 1 + 2j
+b = 2.3 + 3.2j
+print(type(a), type(b))
+print(a + b)
+print(a * b)
+print(a.conjugate())
+print(b.conjugate())
