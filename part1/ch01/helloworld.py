e944efdb8d6c6e7cc12d8e7fd945b4ed396444c5
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2023-10-18 13:58:27 -0400

    Create helloworld.py

---
+++ part1/ch01/helloworld.py
@@ -0,0 +1,15 @@
+# Outputs
+
+# this is the comment line
+# print("hello world from program\n")
+# print("hello", "world", "with", 'certain', "separation", sep=" *** ")
+
+# input()
+
+# Inputs
+
+num = input("Plesae enter your student ID: ")
+print("you have input your ID as: ", num)
+
+# use id() to print memory address of your variable
+print(id(num))
