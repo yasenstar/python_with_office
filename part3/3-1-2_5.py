5bcad208fd1a830f7b10147e34ef316a6f120c00
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2023-10-25 16:59:45 -0400

    update 012

---
+++ part3/3-1-2_5.py
@@ -0,0 +1,4 @@
+my_list = [2,3,66,5,33,22,69]
+new_list = [i for i in my_list if i % 2 == 0]
+print(my_list)
+print(new_list)
---
+++ part3/3-1-2_6.py
@@ -0,0 +1,5 @@
+password = input("input your password: ")
+
+print("your input password is: ", password)
+result = "password correct" if password == "12345" else "password is wrong"
+print(result)
