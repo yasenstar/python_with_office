b3f8af7325323dc876eac98b417c44c27fc93184
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2023-12-11 20:43:27 -0500

    add 8-3

---
+++ part2_Use-Python-in-Office/ch08_work-with-PPT/8-3_unit-of-length/8-3-2_unit-conversion.py
@@ -0,0 +1,17 @@
+from pptx.util import Inches
+
+length = Inches(1)
+
+print(length)
+
+print(length.emu)
+
+print(length.inches)
+
+print(length.cm)
+
+print(length.mm)
+
+print(length.pt)
+
+print(length.centipoints)
