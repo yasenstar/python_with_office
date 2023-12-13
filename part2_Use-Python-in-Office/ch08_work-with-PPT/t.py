b59aff6da8bd4014a5cf3cc6ab5fc8e5658a5b7e
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2023-12-13 13:22:26 -0500

    Delete t.py

--- part2_Use-Python-in-Office/ch08_work-with-PPT/t.py
+++
@@ -1,9 +0,0 @@
-from pptx import Presentation
-from pptx.enum.chart import XL_CHART_TYPE
-ppt = Presentation()
-
-styles = ppt.styles
-chart_styles = [s for s in styles if s.type == XL_CHART_TYPE.__members__]
-
-for style in chart_styles:
-    print(style.name)
