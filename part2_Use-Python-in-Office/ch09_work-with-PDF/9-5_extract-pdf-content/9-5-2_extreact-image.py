981e7616213a0788faa7dbfbe428de96f3797621
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2023-12-21 23:06:33 -0500

    add 9-5 part 2

---
+++ part2_Use-Python-in-Office/ch09_work-with-PDF/9-5_extract-pdf-content/9-5-2_extreact-image.py
@@ -0,0 +1,21 @@
+import os
+import fitz
+
+pdf = fitz.open("./image.pdf")
+
+save_path = "./pdf_images"
+
+for index, page in enumerate(pdf.pages()):
+    print(f"load the No. {index+1} page successfully!")
+    for image in page.get_images():
+        xref = image[0]
+        print(xref)
+
+        pix = fitz.Pixmap(pdf, xref)
+
+        if not os.path.exists(save_path):
+            os.mkdir(save_path)
+
+        path = os.path.join(save_path, f"page{index+1}_{xref}.png")
+
+        pix.save(path)
